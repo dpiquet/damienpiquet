96a4f32137968fc4c8fe9a59b2e9e108fc29ad3b
Damien PIQUET <piqudam@gmail.com>
2025-04-27 21:45:26 +0200

    update 3d animation

--- js/animation.js
+++ js/animation.js
@@ -1,107 +1,78 @@
-if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-    // Mobile device style: fill the whole browser client area with the game canvas:
-    const meta = document.createElement('meta');
-    meta.name = 'viewport';
-    meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
-    document.getElementsByTagName('head')[0].appendChild(meta);
+window.addEventListener('DOMContentLoaded', event => {
 
-    const canvas = document.querySelector("#background-animation");
-}
+    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+        // Mobile device style: fill the whole browser client area with the game canvas:
+        const meta = document.createElement('meta');
+        meta.name = 'viewport';
+        meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+        document.getElementsByTagName('head')[0].appendChild(meta);
 
-let selectedScene = 'root'
-let myGameInstance = null;
+        const canvas = document.querySelector("#background-animation");
+    }
 
-function callSceneChange() {
-    if (myGameInstance)
+    let selectedScene = 'root'
+    let myGameInstance = null;
+
+    function callSceneChange() {
+        if (myGameInstance)
+            myGameInstance.SendMessage('api', 'setCamera', selectedScene);
+    }
+
+    function setScene(newScene) {
+        if (selectedScene === newScene) return;
+        selectedScene = newScene;
+        callSceneChange();
+    }
+
+    var controller = new ScrollMagic.Controller({
+        // addIndicators: true
+    });
+
+    new ScrollMagic.Scene({triggerElement: '#root'})
+        .addTo(controller)
+        .on("start", function () {
+            setScene('root');
+        });
+    new ScrollMagic.Scene({triggerElement: '#about'})
+        .addTo(controller)
+        .on("start", function () {
+            setScene('desk');
+        });
+    new ScrollMagic.Scene({triggerElement: '#about-bottom'})
+        .addTo(controller)
+        .on("start", function () {
+            setScene('desk');
+        });
+
+    new ScrollMagic.Scene({triggerElement: '#projects'})
+        .addTo(controller)
+        .on("start", function () {
+            setScene('showroom');
+        });
+    new ScrollMagic.Scene({triggerElement: '#projects-bottom'})
+        .addTo(controller)
+        .on("start", function () {
+            setScene('showroom');
+        });
+
+    new ScrollMagic.Scene({triggerElement: '#contact'})
+        .addTo(controller)
+        .on("start", function () {
+            setScene('contact');
+        });
+    createUnityInstance(document.querySelector("#background-animation"), {
+        arguments: [],
+        dataUrl: "../assets/3d/build.data.unityweb",
+        frameworkUrl: "../assets/3d/build.framework.js.unityweb",
+        codeUrl: "../assets/3d/build.wasm.unityweb",
+        streamingAssetsUrl: "StreamingAssets",
+        companyName: "DamienPIQUET",
+        productName: "portfolio",
+        productVersion: "0.1.0",
+        // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
+        // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
+    }).then(unityInstance => {
+        myGameInstance = unityInstance;
         myGameInstance.SendMessage('api', 'setCamera', selectedScene);
-}
-
-function setScene(newScene) {
-    if (selectedScene === newScene) return;
-    selectedScene = newScene;
-    callSceneChange();
-}
-
-var controller = new ScrollMagic.Controller({
-    // addIndicators: true
+    });
 });
-
-new ScrollMagic.Scene({triggerElement: '#root'})
-    .addTo(controller)
-    .on("start", function () {
-        setScene('root');
-    });
-new ScrollMagic.Scene({triggerElement: '#about'})
-    .addTo(controller)
-    .on("start", function () {
-        setScene('desk');
-    });
-new ScrollMagic.Scene({triggerElement: '#about-bottom'})
-    .addTo(controller)
-    .on("start", function () {
-        setScene('desk');
-    });
-
-new ScrollMagic.Scene({triggerElement: '#projects'})
-    .addTo(controller)
-    .on("start", function () {
-        setScene('showroom');
-    });
-new ScrollMagic.Scene({triggerElement: '#projects-bottom'})
-    .addTo(controller)
-    .on("start", function () {
-        setScene('showroom');
-    });
-
-new ScrollMagic.Scene({triggerElement: '#contact'})
-    .addTo(controller)
-    .on("start", function () {
-        setScene('contact');
-    });
-
-
-// document.addEventListener('activate.bs.scrollspy', function(event){
-//     console.log('scroll event: ', event.relatedTarget.hash);
-//     switch (event.relatedTarget.hash) {
-//         case '#projects':
-//             selectedScene = 'showroom';
-//             break;
-//         case '#about':
-//             selectedScene = 'desk';
-//             break;
-//         case '#contact':
-//             selectedScene = 'contact';
-//             break;
-//     }
-//
-//     callSceneChange();
-// });
-
-
-
-createUnityInstance(document.querySelector("#background-animation"), {
-    arguments: [],
-    dataUrl: "../assets/3d/build.data.unityweb",
-    frameworkUrl: "../assets/3d/build.framework.js.unityweb",
-    codeUrl: "../assets/3d/build.wasm.unityweb",
-    streamingAssetsUrl: "StreamingAssets",
-    companyName: "DamienPIQUET",
-    productName: "portfolio",
-    productVersion: "0.1.0",
-    // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
-    // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
-}).then(unityInstance => {
-    myGameInstance = unityInstance;
-    myGameInstance.SendMessage('api', 'setCamera', selectedScene);
-});
-
-// window.addEventListener('scroll', (e) => {
-//     if (selectedScene === 'root')
-//         return;
-//    
-//     if (window.scrollY < document.documentElement.clientHeight / 4)
-//     {
-//         selectedScene = 'root';
-//         callSceneChange();
-//     }
-// });
